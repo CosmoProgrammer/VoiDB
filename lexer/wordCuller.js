08949fd32beba76990cb29bd975cffacb5a5b42d
CosmoProgrammer <anirudh.mss@gmail.com>
2022-01-12 22:30:08 +0530

    Working on the Word Culler

--- lexer/wordCuller.js
+++ lexer/wordCuller.js
@@ -2,6 +2,32 @@ const path = require('path');
 const classes = require(path.join(__dirname, '../', 'classes.js'));
 
 function wordCuller(code){
+    //List of all Words to be removed from the code
+    const remove = ['a', 'called', 'with', 'the', 'some', 'and', 'by', 'in', 'from', 'new'];
+
+    //Splits by ; but also keeps the ; so that I can merge at the end of the code while keeping the ;
+    let c_sentences = code.split(';');
+    let c=[];
+
+    //Splits the code into words but also makes sure everything inside "" is not split apart.
+    for(var x in c_sentences){c.push(c_sentences[x].match(/\w+|"[^"]+"/g))};
+
+    //If the last word in the split code is empty, then the blank word is removed from the split code.
+    try{ if(c[x].length === 0){console.log("Last element in c is not empty");}} catch(e){let popped = c.pop();};
+
+    //Removes all the words in the remove constant
+    for(var x=0; x<c.length; x++){
+        for(var y=0; y<c[x].length; y++){
+            if(remove.includes(c[x][y])){ 
+                c[x].splice(y,1);
+                y--;
+            }
+        }
+    }
+
+    let mergedArray = [].concat.apply([], c);
+    console.log(mergedArray.join(' '));
+     
     return code;
 }
 
