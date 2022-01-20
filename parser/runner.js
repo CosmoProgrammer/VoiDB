99ea31783636fa311fabd0f8b0f7dd99aa05ba3c
CosmoProgrammer <anirudh.mss@gmail.com>
2022-01-20 23:04:09 +0530

    Completed Default Command

--- parser/runner.js
+++ parser/runner.js
@@ -2,7 +2,8 @@ const actions = {
     create: require(`${__dirname}\\actions\\create\\create.js`),
     use: require(`${__dirname}\\actions\\use\\use.js`),
     delete: require(`${__dirname}\\actions\\delete\\delete.js`),
-    truncate: require(`${__dirname}\\actions\\truncate\\truncate.js`)
+    truncate: require(`${__dirname}\\actions\\truncate\\truncate.js`),
+    default: require(`${__dirname}\\actions\\default\\default.js`)
 }
 
 function run(code, preRunData){
@@ -14,6 +15,8 @@ function run(code, preRunData){
         return actions.delete(code, preRunData);
     } else if(code.action==='truncate'){
         return actions.truncate(code, preRunData);
+    } else if(code.action==='default'){
+        return actions.default(code, preRunData);
     }
     return code.action;
 }
