d6fbba3f4f79724ec3b30a2c641a3d62247bc3b4
CosmoProgrammer <anirudh.mss@gmail.com>
2022-01-20 19:39:34 +0530

    Finished Truncate

---
+++ parser/actions/truncate/truncate.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const fs = require('fs');
+const classes = require(path.join(__dirname, '../../../', 'classes.js'));
+
+function actionTruncate(code, preRunData){
+    let storagePath = path.join(__dirname, '../../../', 'storage', `${preRunData[1]}`, `${code.details.table}.json`);
+    try{
+        let table = require(storagePath);
+        table.values = [];
+        let USING_DATABASE = preRunData[1];
+        if(USING_DATABASE===''){return "KeyError: Not using any database currently"};
+        fs.writeFileSync(storagePath, JSON.stringify(table, null, 4));
+        if(preRunData[0]) return new classes.Success(`Successfully truncated the table ${table.name}`);
+    } catch(e){
+        return new classes.Error("StorageError", `Cannot find table ${code.details.table}`);
+    }
+}
+
+module.exports = actionTruncate;
--- parser/runner.js
+++ parser/runner.js
@@ -1,7 +1,8 @@
 const actions = {
     create: require(`${__dirname}\\actions\\create\\create.js`),
     use: require(`${__dirname}\\actions\\use\\use.js`),
-    delete: require(`${__dirname}\\actions\\delete\\delete.js`)
+    delete: require(`${__dirname}\\actions\\delete\\delete.js`),
+    truncate: require(`${__dirname}\\actions\\truncate\\truncate.js`)
 }
 
 function run(code, preRunData){
@@ -11,6 +12,8 @@ function run(code, preRunData){
         return actions.use(code, preRunData);
     } else if(code.action==="delete"){
         return actions.delete(code, preRunData);
+    } else if(code.action==='truncate'){
+        return actions.truncate(code, preRunData);
     }
     return code.action;
 }
