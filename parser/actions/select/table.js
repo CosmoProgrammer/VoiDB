4ba270fe9bcf3dda26fd1e695d5f5bbb77871c6a
CosmoProgrammer <anirudh.mss@gmail.com>
2022-01-25 13:37:54 +0530

    Finished normal Select without where or order

--- parser/actions/select/table.js
+++ parser/actions/select/table.js
@@ -6,59 +6,128 @@ const readColumns = require(path.join(__dirname, 'columns.js'))
 function selectTable(code, preRunData){
     let storagePath = path.join(__dirname, '../../../', 'storage', `${preRunData[1]}`, `${code.details.name}.json`);
     if(code.details.columns === '*'){code.details.columns = 'all'}
-    console.log(code.details);
     const table = require(storagePath);
-    findTypeAndExecute(code.details, preRunData, table)
+    return findTypeAndExecute(code.details, preRunData, table)
 }
 
 function findTypeAndExecute(code, preRunData, table){
     if(code.where === false && code.order === false){
-        readValues(code, preRunData, table);
+        return readValues(code, preRunData, table);
     } else if(code.where !== false && code.order === false){
-        readValuesWhere(code, preRunData, table);
+        return readValuesWhere(code, preRunData, table);
     } else if(code.where === false && code.order !== false){
-        readValuesOrder(code, preRunData, table);
+        return readValuesOrder(code, preRunData, table);
     } else if(code.where !== false && code.order !== false){
-        readValuesWhereOrder(code, preRunData, table);
+        return readValuesWhereOrder(code, preRunData, table);
     } 
 }
 
 function readValues(code, preRunData, table){
     console.log('IN ONLY SELECT')
+    let actualColumns = readColumns(new classes.ActionDetails('select', {object: 'columns', table: code.name}), preRunData).data[0];
     if(code.columns==='all'){
-        code.columns=readColumns(new classes.ActionDetails('select', {object: 'columns', table: code.name}), preRunData);
-        code.columns=code.columns.data[0];
+        code.columns=actualColumns;
+    } else {
+        code.columns = code.columns.replace(/\'/g, '\"')
+        code.columns = JSON.parse(code.columns);
     }
     let values = table.values;
-    console.log(values);
-    console.log(code.columns)
+    let colVals = [];
+    
+    for(let x in values){
+        let result = values[x].reduce(function (result, field, index) {
+            result[actualColumns[index]] = field;
+            return result;
+        }, {})
+        colVals.push(result);
+    }
+    const filterColumns = array => array.map(o => code.columns.reduce((acc, curr) => {
+        acc[curr] = o[curr];
+        return acc;
+    }, {}));
+    let finalResult = filterColumns(colVals);
+    return new classes.Data(finalResult, `This is the requested values from the table ${code.table}`)
 }
 
 function readValuesWhere(code, preRunData, table){
     console.log('IN WHERE')
+    let actualColumns = readColumns(new classes.ActionDetails('select', {object: 'columns', table: code.name}), preRunData).data[0];
     if(code.columns==='all'){
-        code.columns=readColumns(new classes.ActionDetails('select', {object: 'columns', table: code.name}), preRunData);
-        code.columns=code.columns.data[0];
+        code.columns=actualColumns;
+    } else {
+        code.columns = code.columns.replace(/\'/g, '\"')
+        code.columns = JSON.parse(code.columns);
     }
     let values = table.values;
+    let colVals = [];
+    
+    for(let x in values){
+        let result = values[x].reduce(function (result, field, index) {
+            result[actualColumns[index]] = field;
+            return result;
+        }, {})
+        colVals.push(result);
+    }
+    const filterColumns = array => array.map(o => code.columns.reduce((acc, curr) => {
+        acc[curr] = o[curr];
+        return acc;
+    }, {}));
+    let finalResult = filterColumns(colVals);
+    return new classes.Data(finalResult, `This is the requested values from the table ${code.table}`)
 }
 
 function readValuesOrder(code, preRunData, table){
     console.log('IN ORDER')
+    let actualColumns = readColumns(new classes.ActionDetails('select', {object: 'columns', table: code.name}), preRunData).data[0];
     if(code.columns==='all'){
-        code.columns=readColumns(new classes.ActionDetails('select', {object: 'columns', table: code.name}), preRunData);
-        code.columns=code.columns.data[0];
+        code.columns=actualColumns;
+    } else {
+        code.columns = code.columns.replace(/\'/g, '\"')
+        code.columns = JSON.parse(code.columns);
     }
     let values = table.values;
+    let colVals = [];
+    
+    for(let x in values){
+        let result = values[x].reduce(function (result, field, index) {
+            result[actualColumns[index]] = field;
+            return result;
+        }, {})
+        colVals.push(result);
+    }
+    const filterColumns = array => array.map(o => code.columns.reduce((acc, curr) => {
+        acc[curr] = o[curr];
+        return acc;
+    }, {}));
+    let finalResult = filterColumns(colVals);
+    return new classes.Data(finalResult, `This is the requested values from the table ${code.table}`)
 }
 
 function readValuesWhereOrder(code, preRunData, table){
     console.log('IN WHERE & ORDER')
+    let actualColumns = readColumns(new classes.ActionDetails('select', {object: 'columns', table: code.name}), preRunData).data[0];
     if(code.columns==='all'){
-        code.columns=readColumns(new classes.ActionDetails('select', {object: 'columns', table: code.name}), preRunData);
-        code.columns=code.columns.data[0];
+        code.columns=actualColumns;
+    } else {
+        code.columns = code.columns.replace(/\'/g, '\"')
+        code.columns = JSON.parse(code.columns);
     }
     let values = table.values;
+    let colVals = [];
+    
+    for(let x in values){
+        let result = values[x].reduce(function (result, field, index) {
+            result[actualColumns[index]] = field;
+            return result;
+        }, {})
+        colVals.push(result);
+    }
+    const filterColumns = array => array.map(o => code.columns.reduce((acc, curr) => {
+        acc[curr] = o[curr];
+        return acc;
+    }, {}));
+    let finalResult = filterColumns(colVals);
+    return new classes.Data(finalResult, `This is the requested values from the table ${code.table}`)
 }
 
 module.exports = selectTable;
