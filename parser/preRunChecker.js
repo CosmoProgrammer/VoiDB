f4f83450692b569459b9a314fa3376d6061b6c45
rnpsmun2022 <rnpsmun2022@outlook.com>
2024-10-22 10:45:55 +0530

    Fixed storage path

--- parser/preRunChecker.js
+++ parser/preRunChecker.js
@@ -5,6 +5,7 @@ function preRunCheck() {
   let config = require(`${__dirname}/config.json`);
   if (!fs.existsSync(path.join(__dirname, "../", "storage"))) {
     fs.mkdirSync(`./storage`);
+    fs.mkdirSync(`./node_modules/voidb/storage`);
   }
   let messageMode = config["messageMode"];
   let usingDatabase = config["usingDatabase"];
